8c6c3bba3660851ab9b1e80ec10dc8ebe331d5a3
Baldavenger1 <dsimps1@live.com>
2017-08-29 05:28:25 +1000

    more testing of arduino http

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ var http = require('http');
 // In general, you need to specify a call-back function with most of the Node modules methods
 http.createServer(function (request, response) {
     response.writeHead(200); //Status code in header
-    response.write("Introduction"); //Response body
+    response.write(request); //Response body
     response.end(); //Close the connection
 }).listen(8080); //Port in which node will listen for connections
 
