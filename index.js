3d7f29668b7ba03b9d1f00950091f31e64da5a75
Baldavenger1 <dsimps1@live.com>
2017-08-31 06:22:43 +1000

    more testifying

--- index.js
+++ index.js
@@ -36,6 +36,7 @@
 // In this example, we will create a node server and serve an HTTP response
 
 // Use the 'require' keyword to load modules(libraries)
+var bbb = require('request');
 var http = require('http');
 var url = require('url');
 // In general, you need to specify a call-back function with most of the Node modules methods
@@ -49,7 +50,11 @@ http.createServer(function (request, response) {
     response.end(); //Close the connection
 	
 }).listen(8080); //Port in which node will listen for connections
-
+request('http://14.202.147.3:8888', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the homepage. 
+});)
 console.log('Listening on port 8080...');
 
 /* Run the following cmd to start node and run the server
